bd3f0c1e67ab22a6f648899c120505286e5781ee
herminioaureo <herminio.aureo@outlook.com>
2023-04-07 11:46:29 -0300

    Incluiding basic configuration of Spring Boot, generated by Spring Initializr

---
+++ src/test/java/com/herminio/carcatalog/CarcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herminio.carcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
